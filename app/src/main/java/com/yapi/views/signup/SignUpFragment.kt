40ab83784d3269ab88fe4ca702cd33ca051f19f1
daman <damandhiman78880@gmail.com>
2023-03-07 12:38:13 +0530

    sigin screen and sinup opt code screen

--- app/src/main/java/com/yapi/views/signup/SignUpFragment.kt
+++ app/src/main/java/com/yapi/views/signup/SignUpFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.yapi.R
 import com.yapi.databinding.FragmentSignUpBinding
 
@@ -25,7 +26,9 @@ class SignUpFragment : Fragment() {
 
     private fun init() {
         binding.apply {
-
+            btnSignUp.setOnClickListener {
+                findNavController().navigate(R.id.action_signUpFragment2_to_signUpCodeFragment)
+            }
         }
     }
 }
