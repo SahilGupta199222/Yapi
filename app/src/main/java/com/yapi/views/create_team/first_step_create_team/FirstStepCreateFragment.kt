e1501ff3c8f97cd811fb4f08df5a979f8fc5e06a
daman <damandhiman78880@gmail.com>
2023-03-07 17:43:22 +0530

    Create Team screen.

--- app/src/main/java/com/yapi/views/create_team/first_step_create_team/FirstStepCreateFragment.kt
+++ app/src/main/java/com/yapi/views/create_team/first_step_create_team/FirstStepCreateFragment.kt
@@ -5,19 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.yapi.databinding.FirstStepCreateFragmentBinding
 
 class FirstStepCreateFragment : Fragment() {
     private lateinit var dataBinding: FirstStepCreateFragmentBinding
 
+    val viewModel:FirstStepViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         dataBinding = FirstStepCreateFragmentBinding.inflate(LayoutInflater.from(requireActivity()))
+        dataBinding.vModel=viewModel
         initUI()
-
         return dataBinding.root
     }
 
