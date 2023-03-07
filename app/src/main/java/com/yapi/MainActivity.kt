0788dd090881ef1adabcfd214cc35cba5961563b
daman <damandhiman78880@gmail.com>
2023-03-07 10:55:39 +0530

    navigation implement and signupScreen add

--- app/src/main/java/com/yapi/MainActivity.kt
+++ app/src/main/java/com/yapi/MainActivity.kt
@@ -19,76 +19,31 @@ import javax.inject.Named
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var month: String
     private lateinit var dataBinding: ActivityMainBinding
-    private var btnSave: Button? = null
-    private var tvName: TextView? = null
-
-    @Inject
-    @Named("abed")
-    lateinit var nameValue: String
-
-    @Inject
-    lateinit var prefUtil: PreferenceFile
     val viewModel: DataViewModel by viewModels()
-
-    @Inject
-    lateinit var retrofitAPI: RetrofitAPI
-
-    companion object{
-        var activity:WeakReference<Activity>?=null
-    }
-
-   /* @Inject
-    lateinit  var checkUserStatus:Int*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(dataBinding.root)
-        activity=WeakReference<Activity>(this)
-      //  Log.e("efwfwafefef===",checkUserStatus.toString())
-        month = "02"
+        activity = WeakReference<Activity>(this)
         initUI()
     }
 
-    override fun onStart() {
-        super.onStart()
 
-    }
-
-    private  fun initUI() {
-
-        //supportFragmentManager.beginTransaction().replace(R.id.frameLayout,DeleteGroupFragment()).commit()
-        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, LeaveGroupFragment()).commit()
-
+    private fun initUI() {
 
         dataBinding.apply {
             vModel = viewModel
-            btnfetch.setOnClickListener {
-                var dateValue=prefUtil.fetchStringValue("name")
-                var milisecondValue=convertFromDateToMiliSeconds(dateValue)
-                //hideProgress()
-                Log.e("efefefef===",Constants.BASEURL)
-                Log.e("show_dataaa===",milisecondValue.toString())
-            }
         }
-        dataBinding.tvName.text = prefUtil.fetchStringValue("name")
-        dataBinding.btnSave.setOnClickListener {
-            if(isNetworkConnected()) {
-                viewModel.getCategoryMethod()
-            }else
-            {
-                showMessage(getString(R.string.check_internet_connection))
-            }
-            var mili=System.currentTimeMillis()
-            Log.e("show_dataaa222===",mili.toString())
-            var dateValue=convertMiliToDateFormat(mili)
-            prefUtil.saveStringValue("name", dateValue)
-            Log.e("show_dataaa111===",dateValue.toString())
-            hideKeyboard()
-            showMessage(dataBinding.etNameee.text.toString().trim())
-            //showProgress()
-        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        activity = WeakReference(this@MainActivity)
+    }
+    companion object {
+        var activity: WeakReference<Activity>? = null
     }
 }
