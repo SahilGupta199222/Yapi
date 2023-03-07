d173dbac362a10ff76dce64d321630be0a904d6b
daman <damandhiman78880@gmail.com>
2023-03-07 15:40:51 +0530

    sigin screen and sinup opt code screen

--- app/src/main/java/com/yapi/views/signup_code/SignUpCodeFragment.kt
+++ app/src/main/java/com/yapi/views/signup_code/SignUpCodeFragment.kt
@@ -5,16 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.yapi.R
+import com.yapi.databinding.FragmentSignUpCodeBinding
 
 class SignUpCodeFragment : Fragment() {
-
+    private lateinit var binding:FragmentSignUpCodeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_code, container, false)
+    ): View {
+        binding= FragmentSignUpCodeBinding.inflate(LayoutInflater.from(requireActivity()))
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        onClick()
+    }
+
+    private fun onClick(){
+        binding.apply{
+           btnSignUpCode.setOnClickListener {
+               findNavController().navigate(R.id.action_signUpCodeFragment_to_signupTeam)
+           }
+        }
+    }
 }
