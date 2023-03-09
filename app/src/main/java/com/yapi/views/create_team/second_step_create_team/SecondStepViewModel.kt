c68d91e697053deee03148761aa3aac218e0a758
daman <damandhiman78880@gmail.com>
2023-03-09 19:06:40 +0530

    Implemented large screen layout.

--- app/src/main/java/com/yapi/views/create_team/second_step_create_team/SecondStepViewModel.kt
+++ app/src/main/java/com/yapi/views/create_team/second_step_create_team/SecondStepViewModel.kt
@@ -2,12 +2,15 @@ package com.yapi.views.create_team.second_step_create_team
 
 import android.util.Log
 import android.view.View
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
 import com.yapi.R
 
 class SecondStepViewModel() : ViewModel() {
 
+    var countFieldValue=ObservableField("0/50")
+    var teamNameValue=ObservableField("")
     fun onClick(view: View) {
         when (view.id) {
             R.id.btnSecondCreateTeam -> {
@@ -16,5 +19,10 @@ class SecondStepViewModel() : ViewModel() {
                 view.findNavController().navigate(R.id.action_secondStepCreateTeam_to_thirdStepCreateTeam)
             }
         }
+    }
+
+    fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        Log.w("tag", "onTextChanged $s")
+        countFieldValue.set(s.length.toString()+"/50")
     }
 }
