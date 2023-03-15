135eae815f1a1c3317c924d34035616424dd6a05
daman <damandhiman78880@gmail.com>
2023-03-15 18:35:36 +0530

    richest edit text implemenation

--- app/src/main/java/com/yapi/views/chipset_demo/ChipSetDemoFragment.kt
+++ app/src/main/java/com/yapi/views/chipset_demo/ChipSetDemoFragment.kt
@@ -5,10 +5,12 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import androidx.core.view.size
@@ -18,10 +20,21 @@ import com.yapi.R
 import com.yapi.common.getTextSizeValue
 import com.yapi.databinding.FragmentAddPeopleBinding
 import com.yapi.databinding.FragmentChipSetDemoBinding
+import jp.wasabeef.richeditor.RichEditor
 
 
 class ChipSetDemoFragment : Fragment() {
     private lateinit var binding:FragmentChipSetDemoBinding
+    private var fontStyleSelected=false
+    private var boldTxtSelected=false
+    private var italicTxtSelected=false
+    private var underLineTxtSelected=false
+    private var strikeTxtSelected=false
+    private var listNumberTxtSelected=false
+    private var listBulletTxtSelected=false
+    private var leftAlignTxtSelected=false
+    private var rightAlignTxtSelected=false
+    private var centerAlignTxtSelected=false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -37,6 +50,196 @@ class ChipSetDemoFragment : Fragment() {
 
     private fun init() {
         binding.apply {
+            etRichChatDemo.setPlaceholder("Enter msg here")
+//            etRichChatDemo.hint = "Enter msg here"
+            imgTxtStyleChangeIconChatDemo.setOnClickListener {
+                fontStyleSelected=!fontStyleSelected
+                if (fontStyleSelected) {
+                    imgTxtStyleChangeIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                    layoutFontStylesOfEditTextDemoChat.visibility = View.VISIBLE
+                    viewLineFontStylesEditTextDemoChat.visibility = View.VISIBLE
+                }
+                else {
+                    imgTxtStyleChangeIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.medium_grey_color))
+                    layoutFontStylesOfEditTextDemoChat.visibility = View.GONE
+                    viewLineFontStylesEditTextDemoChat.visibility = View.GONE
+                }
+            }
+            imgBoldTxtIconChatDemo.setOnClickListener {
+                etRichChatDemo.setBold()
+//                etRichChatDemo.focusEditor()
+//                etRichChatDemo.updateTextStyle(EditorTextStyle.BOLD)
+                boldTxtSelected=!boldTxtSelected
+                if (boldTxtSelected) {
+                    imgBoldTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                }
+                else {
+                    imgBoldTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+            }
+            imgItalicTxtIconChatDemo.setOnClickListener {
+                etRichChatDemo.setItalic()
+//                etRichChatDemo.focusEditor()
+
+                italicTxtSelected=!italicTxtSelected
+                if (italicTxtSelected) {
+                    imgItalicTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                }
+                else {
+                    imgItalicTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+            }
+            imgUnderLineTxtIconChatDemo.setOnClickListener {
+                    etRichChatDemo.setUnderline()
+//                etRichChatDemo.focusEditor()
+                underLineTxtSelected=!underLineTxtSelected
+                if (underLineTxtSelected) {
+                    imgUnderLineTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                }
+                else {
+                    imgUnderLineTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+            }
+            imgStrikeTxtIconChatDemo.setOnClickListener {
+                    etRichChatDemo.setStrikeThrough()
+//                etRichChatDemo.focusEditor()
+                strikeTxtSelected=!strikeTxtSelected
+
+                if (strikeTxtSelected) {
+                    imgStrikeTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                }
+                else {
+                    imgStrikeTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+            }
+            imgFormatListNumberTxtIconChatDemo.setOnClickListener {
+//                etRichChatDemo.focusEditor()
+                    etRichChatDemo.setNumbers()
+                listNumberTxtSelected=!listNumberTxtSelected
+
+                if (listNumberTxtSelected) {
+                    imgFormatListNumberTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                }
+                else {
+                    imgFormatListNumberTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+            }
+            imgFormatListBulletedTxtIconChatDemo.setOnClickListener {
+                    etRichChatDemo.setBullets()
+                listBulletTxtSelected=!listBulletTxtSelected
+                if (listBulletTxtSelected) {
+                    imgFormatListBulletedTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                }
+                else {
+                    imgFormatListBulletedTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+            }
+            imgLeftAlignTxtIconChatDemo.setOnClickListener {
+                    etRichChatDemo.setAlignLeft()
+                leftAlignTxtSelected=!leftAlignTxtSelected
+                if (leftAlignTxtSelected) {
+                    imgLeftAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                }
+                else {
+                    imgLeftAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+                if(centerAlignTxtSelected){
+                    centerAlignTxtSelected=false
+                    imgCenterAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+                if(rightAlignTxtSelected){
+                    rightAlignTxtSelected=false
+                    imgRightAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+            }
+            imgCenterAlignTxtIconChatDemo.setOnClickListener {
+                    etRichChatDemo.setAlignCenter()
+                centerAlignTxtSelected=!centerAlignTxtSelected
+                if (centerAlignTxtSelected) {
+                    imgCenterAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                }
+                else {
+                    imgCenterAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+                if(leftAlignTxtSelected){
+                    leftAlignTxtSelected=false
+                    imgLeftAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+                if(rightAlignTxtSelected){
+                    rightAlignTxtSelected=false
+                    imgRightAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+            }
+            imgRightAlignTxtIconChatDemo.setOnClickListener {
+                    etRichChatDemo.setAlignRight()
+                rightAlignTxtSelected=!rightAlignTxtSelected
+                if (rightAlignTxtSelected) {
+                    imgRightAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueColor))
+                }
+                else {
+                    imgRightAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+                if(leftAlignTxtSelected){
+                    leftAlignTxtSelected=false
+                    imgLeftAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+                if(centerAlignTxtSelected){
+                    centerAlignTxtSelected=false
+                    imgCenterAlignTxtIconChatDemo.setColorFilter(ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGrey))
+                }
+            }
             etChips.doOnTextChanged { text, start, before, count ->
                 if(text?.isNotEmpty() == true ){
                     layoutAddPeople.visibility=View.VISIBLE
@@ -44,6 +247,7 @@ class ChipSetDemoFragment : Fragment() {
                     txtUserNameChip.text= text[0].toString()
                 }else{
                     layoutAddPeople.visibility=View.GONE
+                    etRichChatDemo.removeFormat()
                 }
             }
             layoutAddPeople.setOnClickListener {
@@ -54,9 +258,25 @@ class ChipSetDemoFragment : Fragment() {
                     chipGroup.visibility=View.VISIBLE
                 }
             }
-            imgItally.setOnClickListener {
-                editor.setItalic()
+            imgLinkIconChatDemo.setOnClickListener {
+                etRichChatDemo.insertLink("https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg","iron man link")
             }
+
+            etRichChatDemo.setOnDecorationChangeListener { text, types ->
+                Log.i("asdfjnasdf","i ->${types}  and text  $text\n")
+            }
+            etRichChatDemo.setOnDecorationChangeListener(object : RichEditor.OnDecorationStateListener{
+                override fun onStateChangeListener(
+                    text: String?,
+                    types: MutableList<RichEditor.Type>?,
+                ) {
+                }
+
+
+            })
+
+
+
         }
     }
 
