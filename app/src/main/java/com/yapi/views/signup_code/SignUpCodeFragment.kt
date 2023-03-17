9b91969de65feab6f9271ea8405b5e8f2bfe683c
Sahil <sfs.sahil20@gmail.com>
2023-03-17 12:19:53 +0530

    Removed Password field from Login screen.

--- app/src/main/java/com/yapi/views/signup_code/SignUpCodeFragment.kt
+++ app/src/main/java/com/yapi/views/signup_code/SignUpCodeFragment.kt
@@ -39,7 +39,7 @@ class SignUpCodeFragment : Fragment() {
     }
 
     private fun onClick(){
-       // binding.otpViewSignUpCodee as Fram
+        //binding.otpViewSignUpCodee.
         binding.apply{
           /* btnSignUpCode.setOnClickListener {
                findNavController().navigate(R.id.action_signUpCodeFragment_to_signupTeam)
